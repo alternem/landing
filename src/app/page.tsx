7883b5a71f7c7f20aa934a4b8ebae374fb6002d5
Alternem <165897885+alternem@users.noreply.github.com>
2025-12-01 17:56:31 -0700

    Add gap between main content and link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import { Simple } from "@/components/Simple";
 export default function Home() {
   return (
     <div className="flex h-screen w-screen">
-      <main className="flex h-full w-full">
+      <main className="flex h-full w-full flex-col gap-2">
           <Simple 
             title="Alternem"
             subtitle="Inc"
             tagLine="Be Chill, Be Creative and Have fun"
             className="bg-black text-neutral-600"
             />
-          <a href="http://www.createvideo.cloud" className="w-full mt-6"> Make gifts with CreateVideo.cloud by Alternem </a>
+            <a href="http://www.createvideo.cloud" className="w-full mt-6 bg-black text-neutral-600"> Make gifts with CreateVideo.cloud by Alternem </a>
       </main>
     </div>
   );
